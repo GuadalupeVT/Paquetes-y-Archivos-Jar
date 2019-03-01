e980d8ad38e3e29c361d35c02c70959a01250a68
Guadalupe Vazquez <guvadlt@gmail.com>
2019-03-01 17:41:59 -0600

    se agregaron los metodos para calcular el volumen y area a la piramide

--- src/edu/tecjerez/topicos/figuras/tresDimensiones/Piramide.java
+++ src/edu/tecjerez/topicos/figuras/tresDimensiones/Piramide.java
@@ -1,10 +1,25 @@
 package edu.tecjerez.topicos.figuras.tresDimensiones;
 
+import edu.tecjerez.topicos.figuras.Triangulo;
+import edu.tecjerez.topicos.geometria.Punto;
+
 public class Piramide {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private double areaBase (double lado) {
+		return (4*lado);
+	}
+	
+	private double areaLateral (double lado1,double lado2,double lado3) {
+		Triangulo a= new Triangulo();
+		return (a.area(lado1,lado2,lado3)*4);
+	}
+	
+	double voulumen (double lado, double altura) {
+		return (areaBase(lado)*altura)/3;
+	}
+	public double areaTotal(double lado1,double lado2,double lado3, double base) {
+		return (areaBase(base)+areaLateral(lado1,lado2,lado3));
 	}
 
 }
+
