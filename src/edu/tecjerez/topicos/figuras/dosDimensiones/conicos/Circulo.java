0e08f9512340c285026a6be2f248fc3c416b797d
Guadalupe Vazquez <guvadlt@gmail.com>
2019-03-01 17:53:20 -0600

    Se agrego area y perimetro al circulo

--- src/edu/tecjerez/topicos/figuras/dosDimensiones/conicos/Circulo.java
+++ src/edu/tecjerez/topicos/figuras/dosDimensiones/conicos/Circulo.java
@@ -1,5 +1,13 @@
 package edu.tecjerez.topicos.figuras.dosDimensiones.conicos;
 
 public class Circulo {
+	
+	public double area (double radio) {
+		return Math.PI*(radio*radio);
+	}
+	
+	public double perimetro(double radio) {
+		return 2*Math.PI*radio;
+	}
 
 }
