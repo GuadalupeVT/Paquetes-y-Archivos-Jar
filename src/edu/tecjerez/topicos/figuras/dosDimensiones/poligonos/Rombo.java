d84754e09600bec7defde054b6d54b1ea97e6937
Guadalupe Vazquez <guvadlt@gmail.com>
2019-03-01 18:08:18 -0600

    Se agregaron los metodos area y perimetro a la clase rombo

--- src/edu/tecjerez/topicos/figuras/dosDimensiones/poligonos/Rombo.java
+++ src/edu/tecjerez/topicos/figuras/dosDimensiones/poligonos/Rombo.java
@@ -1,5 +1,12 @@
 package edu.tecjerez.topicos.figuras.dosDimensiones.poligonos;
 
 public class Rombo {
+	
+	double perimetro (double D, double d) {
+		return (D*d)/2;
+	}
+	double area (double lado) {
+		return lado*2;
+	}
 
 }
