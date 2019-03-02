704848b103d91060ab6216055c090ffb29a5b203
Guadalupe Vazquez <guvadlt@gmail.com>
2019-03-01 18:11:41 -0600

    se agregaron los metodos area y perimetro a la clase elipse

--- src/edu/tecjerez/topicos/figuras/dosDimensiones/conicos/Elipse.java
+++ src/edu/tecjerez/topicos/figuras/dosDimensiones/conicos/Elipse.java
@@ -1,5 +1,11 @@
 package edu.tecjerez.topicos.figuras.dosDimensiones.conicos;
 
 public class Elipse {
+	public double area(double a, double b) {
+		return Math.PI*a*b;
+	}
+	public double perimetro(double a, double b) {
+		return Math.PI*(a+b);
+	}
 
 }
