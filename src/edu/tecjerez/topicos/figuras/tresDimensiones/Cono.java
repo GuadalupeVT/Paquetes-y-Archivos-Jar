611eeaf134a57f190894f6fdb94e837d3a1eb44c
Guadalupe Vazquez <guvadlt@gmail.com>
2019-03-01 18:03:17 -0600

    Se agregaron los metodos a la clase cono

--- src/edu/tecjerez/topicos/figuras/tresDimensiones/Cono.java
+++ src/edu/tecjerez/topicos/figuras/tresDimensiones/Cono.java
@@ -1,5 +1,22 @@
 package edu.tecjerez.topicos.figuras.tresDimensiones;
 
-public class Cono {
+import edu.tecjerez.topicos.figuras.dosDimensiones.conicos.Circulo;
 
+public class Cono {
+	protected double areaBase(double radio) {
+		Circulo c=new Circulo();
+		return c.area(radio);
+	}
+	
+	public double volumen(double radio, double altura) {
+		return((areaBase(radio)*altura)/3);
+	}
+
+	protected double areaLateral(double r, double g) {
+		return Math.PI*r*g;
+	}
+	
+	public double areaTotal (double radio,double g) {
+		return areaBase(radio)+areaTotal(radio, g);
+	}
 }
